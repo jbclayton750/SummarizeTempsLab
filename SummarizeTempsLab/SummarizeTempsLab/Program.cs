92bd0a00a792e1a7debf10cd79c0b0cb5199eaad
joseph.clayton750 <59981818+jbclayton750@users.noreply.github.com>
2020-02-08 22:25:36 -0600

    Update Program.cs

--- SummarizeTempsLab/SummarizeTempsLab/Program.cs
+++ SummarizeTempsLab/SummarizeTempsLab/Program.cs
@@ -8,6 +8,36 @@ namespace SummarizeTempsLab
         static void Main(string[] args)
         {
             // temperature data is in temps.txt
+            string filePath = "temps.txt";
+            
+            if (File.Exists(filePath))
+            {
+
+                using (StreamReader sr = File.OpenText(filePath)) 
+                { string line = "enter temperature threshold";
+                
+                while  ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("The file does not exist");
+                Console.ReadLine();
+            }
+                //under if
+                // ask the user to enter the temperature threshold                                                       
+                // open the file and create StreamReader
+                // read a line into a string variable
+                // while the line is not null
+                    // convert (parse) into an integer variable
+                    // add the temperature to my summing variable
+                    // if the current temperature value >= threshold
+                        // increment "above" counter by 1
+                    // else temperature is below
+                        // increment "below" counter by 1
         }
     }
 }
